76a22d731fdfbe5bc90b94f837935bc9df41d0a5
zhouharvey <zhoufengboharvey@gmail.com>
2025-12-01 12:06:56 -0800

    Delete ProteinGPT-main/ProteinGPT-main directory

--- ProteinGPT-main/ProteinGPT-main/src/data/DATASET.md
+++
@@ -1,2 +0,0 @@
-# Datasets
-- We would like to acknowledge the datasets curated by ProteinGPT: Towards Enabling ChatGPT-Like Capabilities on Protein 3D Structures.
--- ProteinGPT-main/ProteinGPT-main/train.sh
+++
@@ -1,3 +0,0 @@
-export MODEL=Meta-Llama-3-8B-Instruct-hf/
-mkdir -p minigpt4/ft/$MODEL
-/home/ubuntu/miniconda3/envs/prot/bin/torchrun --master_port 28888 protein_gpt.py --cfg-path configs/train_instruction_tuning.yaml
